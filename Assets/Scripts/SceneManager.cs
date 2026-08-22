493905ddd627a08b5cf44a3f32c3255b372735e4
Jeff-OwensIyalekhue <j.oiyalekhue@hotmail.de>
2026-08-22 11:29:15 +0200

    SceneManager added

---
+++ Assets/Scripts/SceneManager.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "SceneManager", menuName = "Scriptable Objects/SceneManager")]
+public class SceneManager : ScriptableObject
+{
+    public void LoadScene(string sceneName)
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(sceneName);
+    }
+}
